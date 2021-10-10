ea8c6a5636fbf4ba96ffe2b73ecbca57f3a750ed
abdin20 <naymul@hotmail.ca>
2021-10-10 18:36:54 -0400

    can navigate from game to menu

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -1,19 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PauseScript : MonoBehaviour
 {
 
-    bool isPaused;
+    public GameObject canvas;
+
     void Start()
-    {   
-        isPaused=false;
+    {
+        Time.timeScale=1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if escape is pressed toggle between pause or not
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            togglePause();
+        }
+
+    }
+
+    public void togglePause()
+    {
+        Debug.Log("toggling");
+        //toggle between pausing and not
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            canvas.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            canvas.SetActive(true);
+        }
+
+    }
+
+    public void Quit()
+    {
+        //quit the application
+        Application.Quit();
+    }
+
+    //restart current scene
+    public void Restart(){
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+  public void Menu(){
+
+        //load game scene when play is pressed
+        SceneManager.LoadScene("MainMenu");
+
     }
 }
